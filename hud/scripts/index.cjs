b1ce9a500e68cbe4e5bae7539669025b3d27046b
BeerlD <bellkunmoreanime@gmail.com>
2023-04-11 05:21:30 -0300

    update script.js

--- hud/scripts/index.cjs
+++ hud/scripts/index.cjs
@@ -11,24 +11,21 @@ function setProgressBarValue(progressid, value, speed=1)
     
     progress.style.strokeDashoffset = borderLen;
     progress.style.strokeDasharray = borderLen + ',' + borderLen;
+    value = borderLen - borderLen * (value / 100);
 
     function updateProgressBar()
     {
         if (anim[progressid] == undefined)
             return;
         
-        if (offset - speed < borderLen - borderLen * (value / 100))
+        offset -= speed;
+        if (offset <= value)
         {
-            offset = borderLen - borderLen * (value / 100)
-        } else {
-            offset -= speed;
-        }
-
-        progress.style.strokeDashoffset = offset;
-
-        if (offset <= borderLen - borderLen * (value / 100))
+            progress.style.strokeDashoffset = value;
             return window.cancelAnimationFrame(anim[progressid]);
+        }
         
+        progress.style.strokeDashoffset = offset;
         anim[progressid] = window.requestAnimationFrame(updateProgressBar);
     }
 
